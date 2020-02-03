e52f1c84099af265090a4956b213bf0ca8957726
Chih-Hsuan Huang <chhuang215@gmail.com>
2020-02-03 23:48:09 +0800

    dynamic list all skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -13,10 +13,29 @@ export class SkillsComponent implements OnInit {
     this.skills={
       progLang:["Java","C/C++","C#","Python","Haskell","VB/VBA"],
       db:["MySQL", "MS Access", "MongoDB / NoSQL"],
+      web:{
+        "main":["HTML", "Javascript","CSS", "PHP"],
+        "frameworks":["JQuery", "Node.js", "Angular", "Ionic",
+                      "ReactJS","MeteorJS", "Twitter Bootstrap",
+                      "Material Design","Flask"
+        ],
+        "architecture":["REST", "SOAP"],
+        "other":["AJAX", "JSON","ECMA","Webpack","Gulp" ,"Drupal"],
+      },
+      vcs:["Github", "SVN", "Bitbucket"],
+      other:["Virtualization", 
+        "Shell script", "Docker",".NET Framework",
+        "AWS","Unix","Unit Testing", "IIS", "Arduino"
+      ],
     }
     this.skillIcons={
       "Java": "devicon-java-plain-wordmark colored",
       "HTML": "devicon-html5-plain-wordmark colored",
+      "C/C++": "devicon-c-plain-wordmark colored",
+      "C#":"devicon-csharp-plain colored",
+      "Python":"devicon-python-plain-wordmark colored",
+      
+
     }
   }
 
